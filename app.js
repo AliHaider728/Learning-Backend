a81b2635cb3795812b2c0d3bfb740aca09a3bf37
AliHaider728 <decantboy67@gmail.com>
2025-10-08 01:52:38 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const sum = (a,b )=>console.log(`some of ${a}  and ${b} is = ${a+b} `)
+const Multiply = (a,b )=>console.log(`Multiply of ${a}  and ${b} is = ${a*b} `)
+const minas = (a,b )=>console.log(`minus of ${a}  and ${b} is = ${a*b} `)
+ 
+export  {sum , Multiply ,minas} 
